0fef3e558160423a3ad1b11229ff7a3f286c42f1
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2022-11-04 23:11:07 +0100

    feat: improved input

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Imports
 import json, csv, os, hashlib
 import pandas as pd
 
@@ -6,8 +7,6 @@ n_format = "CHIP-0007"
 collection_name = "Zuri NFT Tickets for Free Lunch"
 collection_description = "Zuri NFT Tickets for Free Lunch"
 collection_id = "b774f676-c1d5-422e-beed-00ef5510c64d"
-
-csv_path = 'csv/main.csv'
 
 
 def hash_jsons(filename):
@@ -20,8 +19,11 @@ def hash_jsons(filename):
 
 def generate_final_output(hash_list):
     '''Appends Hashes to Original CSV'''
-    df = pd.read_csv('csv\main.csv')
+    df = pd.read_csv(csv_path)
     df['Hash'] = hash_list
+    # Directory to hold metadata
+    if not os.path.exists("output"):
+        os.makedirs("output")
     df.to_csv('output/main.output.csv', index=False)
 
 
@@ -42,6 +44,12 @@ def func_main(filepath):
 
     with open(filepath,'r') as csvfile:
         csvreader = list(csv.reader(csvfile))
+
+        # Directory to hold metadata
+        if not os.path.exists("metadata"):
+            os.makedirs("metadata")
+
+        # Loop over the rows of CSV
         for i in range(1, len(csvreader)):
 
             attributes_list = []
@@ -73,7 +81,7 @@ def func_main(filepath):
             jsondata['format'] = n_format
             jsondata['name'] = name
             jsondata['description'] = description
-            jsondata['minting_tool'] = minting_tool or ''
+            jsondata['minting_tool'] = minting_tool
             jsondata['series_number'] = series_number
             jsondata['series_total'] = series_total
             jsondata['sensitive_content'] = sensitive_content
@@ -97,5 +105,12 @@ def clean_csv(csv_path):
 
 
 if __name__ == "__main__":
+    print("Welcome to the NFT Metadata Generator and Hashing Tool")
+    print("=================================================================")
+
+    csv_path = input("Kindly enter the path to the CSV file: ").strip('"')
+    print("Generating metadata and hashes...")
+
     clean_csv(csv_path)
     func_main(csv_path)
+    print(f"Done: {csv_path}.output.csv was generated")
